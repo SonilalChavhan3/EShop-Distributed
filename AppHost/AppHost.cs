d9981502dc6d4c741941ce39eb884d72c10eb64d
Sonilalchavhan <chavhansonilal@gmail.com>
2025-12-25 17:21:14 +0530

    US-02:Product Domain Entity Models in Catalog Microservice Domain Models

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,4 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 // add projects and cloud-native backing services here
 
+builder.AddProject<Projects.Catalog>("catalog");
+// add projects and cloud-native backing services here
+
 builder.Build().Run();
