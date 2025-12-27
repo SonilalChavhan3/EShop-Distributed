a04ae975629a97993d547d2c204f26fbcb503b98
Sonilalchavhan <chavhansonilal@gmail.com>
2025-12-27 13:02:57 +0530

    US-10:Develop SC Domain Entity Models in Basket Microservice Domain Models

---
+++ Basket/Models/ShopingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.Models
+{
+    public class ShopingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShopingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+    }
+}
---
+++ Basket/Models/ShopingCartItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Basket.Models
+{
+    public class ShopingCartItem
+    {
+        public int Quantity { get; set; }= default!;
+
+        public string Color { get; set; }= default!;
+        public int ProductId { get; set; }= default!;
+
+        //willbe come from catlog service
+        public decimal Price { get; set; }= default!;
+        public string ProductName { get; set; } = default!;
+
+    }
+}
