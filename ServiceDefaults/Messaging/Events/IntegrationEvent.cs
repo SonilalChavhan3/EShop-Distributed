fb7cd4981be8094419dbe901423a1a0d5927d265
Sonilalchavhan <chavhansonilal@gmail.com>
2025-12-27 19:13:10 +0530

    US-21:Create Shared Messaging Folders and Classes for ProductPriceChangedIntegration

---
+++ ServiceDefaults/Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDefaults.Messaging.Events
+{
+    public record IntegrationEvent
+    {
+        public Guid EventId { get; }
+        public DateTime OccuredOn { get; }
+        public string EventType { get; }
+
+        public IntegrationEvent()
+        {
+            EventId = Guid.NewGuid();
+            OccuredOn = DateTime.UtcNow;
+            EventType = GetType().AssemblyQualifiedName!;
+        }
+
+
+    }
+}
