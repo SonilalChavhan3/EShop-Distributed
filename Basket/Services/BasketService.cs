57b51f510e801e8342ab900fd54844c32ea21dd0
Sonilalchavhan <chavhansonilal@gmail.com>
2025-12-27 16:59:34 +0530

    US-19:Integrate Catalog into BasketService.cs using CatalogApiClient

--- Basket/Services/BasketService.cs
+++ Basket/Services/BasketService.cs
@@ -1,10 +1,11 @@
-﻿using Basket.Models;
+﻿using Basket.ApiClients;
+using Basket.Models;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text.Json;
 
 namespace Basket.Services
 {
-    public class BasketService(IDistributedCache cache)
+    public class BasketService(IDistributedCache cache,CatalogApiClient apiClient)
     {
         public async Task<ShoppingCart?> GetBasketAsync(string userName)
         {
@@ -14,6 +15,14 @@ namespace Basket.Services
        
         public async Task UpdateBasketAsync(ShoppingCart basket)
         {
+            // Before update(Add/remove Item) into SC, we should call Catalog ms GetProductById method
+            // Get latest product information and set Price and ProductName when adding item into SC
+            foreach (var item in basket.Items)
+            {
+                var product = await apiClient.GetProductsByIdAsync(item.ProductId);
+                item.Price = product.Price;
+                item.ProductName = product.Name;
+            }
             var serializedBasket = JsonSerializer.Serialize(basket);
             await cache.SetStringAsync(basket.UserName, serializedBasket);
         }
