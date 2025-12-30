59c8b1cf49725049b8c6ecf4cf4b23651fcb228f
Sonilalchavhan <chavhansonilal@gmail.com>
2025-12-30 11:30:36 +0530

    US-36:Ollama and Llama Model Hosting Integration .NET Aspire

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -13,14 +13,22 @@ var rabbitMq = builder.AddRabbitMQ("rabbitmq").WithManagementPlugin().WithDataVo
 var keycock = builder.AddKeycloak("keycloak", 8070)
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
-    
+
+var ollama = builder.AddOllama("ollama", 11434)
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent).WithOpenWebUI();
+
+var llama = ollama.AddModel("llama3.2");
+
 
 //Projects
 var catalog = builder.AddProject<Projects.Catalog>("catalog")
     .WithReference(catlogDb)
     .WithReference(rabbitMq)
+    .WithReference(llama)
     .WaitFor(catlogDb)
-    .WaitFor(rabbitMq);
+    .WaitFor(rabbitMq)  
+    .WaitFor(llama);
 
 
 var basket = builder.AddProject<Projects.Basket>("basket")
